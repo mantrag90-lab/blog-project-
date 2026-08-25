9d93e6f1e2f106df180a6cbb2752ceb062ea578a
mantra gupta <mantrag90@gmail.com>
2026-08-26 01:50:36 +0530

    post form added

---
+++ vite-project/src/components/RTE.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Editor } from '@tinymce/tinymce-react';
+import { Controller } from 'react-hook-form';
+
+ export default function RTE({name , control, label, defaultValue=""}) {
+  return (
+ <editor>
+
+ </editor>
+  )
+}
+
+  
